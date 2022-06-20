41a54e8750e2b9a8f2caab823b43790d82530846
Ngoamie <ngomeme28@gmail.com>
2022-06-19 19:45:19 -0700

    changes

---
+++ opcodes_2.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * pint - prints the value at the top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pint(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	while (temp)
+	{
+		if (temp->prev == NULL)
+			break;
+		temp = temp->prev;
+	}
+
+	printf("%d\n", temp->n);
+}
+
+/**
+ * pop - removes the top element of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pop(stack_t **stack, unsigned int nline)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", nline);
+		exit(EXIT_FAILURE);
+	}
+	/* if stack is more than 1 node, else free entire thing */
+	if ((*stack)->next != NULL)
+	{
+		*stack = (*stack)->next;
+		free((*stack)->prev);
+		(*stack)->prev = NULL;
+
